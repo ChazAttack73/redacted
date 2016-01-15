a3a3eb44466609cdd0d1459b7bfcc5a32f263eba
Chaz <cmtlum73@gmail.com>
2016-01-14 20:48:14 -1000

    body-parser uses raw text/plain content type

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
 
-app.use( bodyParser.urlencoded( { extended : true } ) );
+var msg;
+
+app.use( bodyParser.text( { type : 'text/plain' } ) );
+
 
 app.post( '/message', function ( req, res ) {
+  msg = req.body;
+
+  var blacklist = {
+    'selfie' : 'self-portrait',
+    'yummers' : 'delicious',
+    'outchea' : 'are out here',
+    'bruh' : 'wow',
+    'doge' : 'pug',
+    'cilantro' : 'soap',
+    'bae' : 'loved one',
+    'swag' : 'style',
+    'yolo' : 'carpe diem'
+  };
+
   res.send( 'Testes, testes, one...two...three?' );
 });
 
@@ -16,3 +33,5 @@ var server = app.listen( 7777, function () {
 
   console.log( 'What up! What up! We connected yo!' );
 });
+
+
